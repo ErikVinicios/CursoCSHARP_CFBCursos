df15f2a34490e593f41ea75687d49ac3181206b9
Erik Vinicios dos Santos <erik_vinicios_santos@hotmail.com>
2022-09-01 10:08:36 -0300

    01/09/2022
    Estrutura de Repetição FOR

---
+++ Aula19/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aula19
+{
+    class Program
+    {
+        static void Main()
+        {
+           int[] num = new int[10];
+
+           for (int i=0;i<num.Length;i++){
+            num[i]=i;
+           }
+           
+           for (int i=0;i<num.Length;i++){
+            Console.WriteLine($"Valor de num na posição {i}: {num[i]}");
+           }
+        }
+    }
+}
