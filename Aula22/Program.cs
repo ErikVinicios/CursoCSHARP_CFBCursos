318871319bc0f274cf71618157944abba88cd16e
Erik Vinicios dos Santos <erik_vinicios_santos@hotmail.com>
2022-09-02 10:36:07 -0300

    Estrutura de Repetição FOREACH

---
+++ Aula22/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aula22
+{
+    class Program
+    {
+        static void Main()
+        {
+           int[] num=new int[5];
+
+           for (int i=0;i<num.Length;i++){
+            num[i]=i;
+           }
+
+           foreach(int i in num){
+            Console.WriteLine(i);
+           }
+        }
+    }
+}
