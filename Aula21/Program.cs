52eb84dd09d98fe5640f0f9795ef85853bac836b
Erik Vinicios dos Santos <erik_vinicios_santos@hotmail.com>
2022-09-02 10:11:03 -0300

    Estrutura de Repetição DOWHILE

---
+++ Aula21/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aula21
+{
+    class Program
+    {
+        static void Main()
+        {
+           string senha="123";
+           string senhauser;
+           int tentativas=0;
+
+           do{
+            Console.Write("Digite a senha: ");
+            senhauser=Console.ReadLine();
+            tentativas+=1;
+           }while (senha != senhauser);
+            
+            Console.WriteLine("\nSenha Correta ");
+            Console.WriteLine($"Tentativas: {tentativas} ");
+        }
+    }
+}
