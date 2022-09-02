fe7ec071ac4657c03b9e2754668c57491e3098b4
Erik Vinicios dos Santos <erik_vinicios_santos@hotmail.com>
2022-09-02 09:30:33 -0300

    Estrutura de Repetição While

---
+++ Aula20/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aula19
+{
+    class Program
+    {
+        static void Main()
+        {
+           int[] num=new int[10];
+
+           int i=0;
+           while (i<10){
+            num[i]=0;
+            Console.WriteLine(num[i]);
+            i++;
+           }
+           Console.WriteLine("Fim da execução");
+        }
+    }
+}
